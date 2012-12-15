93cc33b5cd7c84a491b471cff1995e1b80712649
Matthew Farrell <farrellm@alum.mit.edu>
2012-12-15 17:52:44 -0500

    add designs directory

---
+++ src/scad_clj/designs/cage.clj
@@ -0,0 +1,14 @@
+(ns scad-clj.designs.cage
+  (:use [scad-clj.scad]))
+
+(write-scad
+   "/home/mfarrell/things/clj/cage.scad"
+   (constant width 18)
+   (constant height width)
+   (sphere :r width)
+   (cylinder :r width :h 9)
+   (module mod [r]
+           (sphere :r r)
+           (cylinder :r 4 :h 2)
+           )
+   )
