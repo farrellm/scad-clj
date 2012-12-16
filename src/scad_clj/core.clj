3a740d674517ad968198abbe71c73b9d33d80637
Matthew Farrell <farrellm@alum.mit.edu>
2012-12-15 19:28:03 -0500

    make consistent with scad.clj

--- src/scad_clj/core.clj
+++ src/scad_clj/core.clj
@@ -9,18 +9,18 @@
 
   (write-scad
    "/home/mfarrell/things/test.scad"
-   (constant 'width 18)
+   (constant width 18)
    (translate [0 2 0]
-              (cylinder :r 'width :h 23))
+              (cylinder width 23))
    (module base [n]
-           (cylinder :r 10 :h 20)
-           (cylinder :r1 10 :r2 30 :h 20)))
+     (cylinder 10 20)
+     ;;(cylinder 10 :r2 30 :h 20)
+     ))
 
-
-  (let [state (ref (make-state))]
-    (alter z)
-    (render @state "/home/mfarrell/things/physics.scad")
-    )
+  ;; (let [state (ref (make-state))]
+  ;;   (alter z)
+  ;;   (render @state "/home/mfarrell/things/physics.scad")
+  ;;   )
   )
  
 (-main)
