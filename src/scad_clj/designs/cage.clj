e5425c7c2b7c46f4531eb934ebb97f85332bc21d
Matthew Farrell <farrellm@alum.mit.edu>
2012-12-18 12:35:12 -0500

    update cage design

--- src/scad_clj/designs/cage.clj
+++ src/scad_clj/designs/cage.clj
@@ -4,15 +4,45 @@
 (write-scad
  "/home/mfarrell/things/clj/cage.scad"
 
- (constant radius 10)
- (constant height radius)
+ (fn! 100)
+ (let [n 3
+       radius 10
+       pillar 2
+       gap 0.1]
 
- (sphere radius)
- (cylinder radius 9)
- (module mod [r]
-   (sphere r)
-   (cylinder 4 2)
+   (defn rot [n r p obj]
+     (concat
+      (map #(rotate [(* %1 (/ 360 n)) [0 0 1]]
+                    (translate [0 r 0]
+                               obj))
+           (range n))
+      ))
+
+   (defn base-hull [n r p h]
+     (hull
+      (rot n r p (cylinder p h))
+      )
+     )
+
+   (defn base [n r p g]
+     (difference
+      (base-hull n (+ r p g) p  p)
+      (base-hull n (+ r (- p) g) p (* 4 p))))
+   
+   (union
+    (translate [0 0 (/ pillar 2)]
+               (union
+                (base n radius pillar gap)
+                (translate [0 0 (* 2 radius)]
+                           (base n radius pillar gap))
+                (translate [0 0 radius]
+                           (rot n (+ radius pillar gap) pillar
+                                (cylinder pillar (* 2 radius))))
+                ))
+    (translate [0 0 radius]
+               (sphere radius))
+    )
    )
- (mod 9)
  )
+ 
 
