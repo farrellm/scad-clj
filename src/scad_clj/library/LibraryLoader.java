b2bb068177c752797cd912daaae6025c792a47f2
Matthew Farrell <farrellm@alum.mit.edu>
2013-04-03 01:12:44 -0400

    fix loader chain

--- src/scad_clj/library/LibraryLoader.java
+++ src/scad_clj/library/LibraryLoader.java
@@ -6,10 +6,12 @@ public class LibraryLoader {
     static {
 	try {
 	    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-	} catch (UnsatisfiedLinkError e) {
-	    System.loadLibrary(Core.NATIVE_LIBRARY_NAME + "_x64");
-	} catch (UnsatisfiedLinkError e) {
-	    System.loadLibrary(Core.NATIVE_LIBRARY_NAME + "_mac");
+	} catch (UnsatisfiedLinkError e1) {
+	    try {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME + "_x64");
+	    } catch (UnsatisfiedLinkError er) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME + "_mac");
+	    }
 	}
     }
 	
