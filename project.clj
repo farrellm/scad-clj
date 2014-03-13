8baf496e3294b6e8cd34fe6dc0d86c588c754747
Matt Adereth <adereth@gmail.com>
2014-03-12 22:14:59 -0400

    Removing OpenCV dependency

--- project.clj
+++ project.clj
@@ -4,10 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.clojure/core.match "0.2.0-alpha12"]
-                 [opencv "2.4.4"]]
-  :repositories {"local" "file:repo"}
-  :jvm-opts ["-Djava.library.path=native"]
-  ;; :jvm-opts ["-Djava.library.path=/usr/local/opencv/share/OpenCV/java"]
-  :java-source-paths ["src"]
+                 [org.clojure/core.match "0.2.0-alpha12"]]
   )
