247300391221668d8a2647b864f42d1a6332fbb5
Matthew Farrel <farrellm@alum.mit.edu>
2014-03-13 10:51:02 -0400

    v0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject scad-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject scad-clj "0.1.0"
+  :description "OpenSCAD DSL in Clojure"
+  :url "https://github.com/farrellm/scad-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.clojure/core.match "0.2.0-alpha12"]]
-  )
+                 [org.clojure/core.match "0.2.0-alpha12"]])
