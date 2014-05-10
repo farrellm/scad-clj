390367d620d3099864f8f546816fc1aeae750e06
Matthew Farrell <farrellm@alum.mit.edu>
2014-05-10 15:45:54 -0400

    add trivial test

--- test/scad_clj/core_test.clj
+++ test/scad_clj/core_test.clj
@@ -1,7 +1,7 @@
 (ns scad-clj.core-test
   (:use clojure.test
-        scad-clj.core))
+        [scad-clj model text scad]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Success"
+    (is (= 0 0))))
