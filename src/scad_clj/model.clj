b02b4615bf7035aa5132db772bb059a39eed6b0a
EricGebhart <e.a.gebhart@gmail.com>
2014-06-13 19:32:22 -0400

    Changed modifier prefix to -.

--- src/scad_clj/model.clj
+++ src/scad_clj/model.clj
@@ -41,10 +41,10 @@
   (if (some #{modifier} [:# :% :* :!])
     `(:modifier ~(name modifier) ~@block)))
 
-(defn |# [& block] (modifier :# block))
-(defn |% [& block] (modifier :% block))
-(defn |* [& block] (modifier :* block))
-(defn |! [& block] (modifier :! block))
+(defn -# [& block] (modifier :# block))
+(defn -% [& block] (modifier :% block))
+(defn -* [& block] (modifier :* block))
+(defn -! [& block] (modifier :! block))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Include & call into Scad libraries
