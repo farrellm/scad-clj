c0e87e0f69bc7eeb467b70a3a6ab1ec9bd716cae
Matthew Farrell <farrellm@alum.mit.edu>
2015-01-01 23:12:10 -0500

    bump dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/farrellm/scad-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.clojure/core.match "0.2.1"]
-                 [net.mikera/core.matrix "0.24.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.match "0.2.2"]
+                 [net.mikera/core.matrix "0.32.1"]
                  ])
