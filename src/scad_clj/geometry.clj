20cb1031cec9bccbbfe58e4d76b589f11042c99c
Matthew Farrell <farrellm@alum.mit.edu>
2015-01-01 23:02:01 -0500

    add end caps to line; add lines

--- src/scad_clj/geometry.clj
+++ src/scad_clj/geometry.clj
@@ -1,18 +1,34 @@
 (ns scad-clj.geometry
-  (:use [clojure.core.matrix :only [distance]]
-        [scad-clj.model]))
+  (:require [clojure.core.matrix :refer [distance]]
+            [scad-clj.model :refer :all :exclude [import use]]))
+
+(def ^:dynamic *line-radius* 1)
 
 (defn line
   "this function will use all the fancy to draw a line from point A to
   point B"
   [from to]
-  (let [diff (map - to from)
-        norm (distance from to)
-        rotate-angle (Math/acos (/ (last diff) norm))
-        rotate-axis [(- (nth diff 1)) (nth diff 0) 0]
-        ]
-    (->> (cylinder 1 norm)
+  (if (= from to)
+    (sphere *line-radius*)
+    (let [diff (map - to from)
+          norm (distance from to)
+          rotate-angle (Math/acos (/ (last diff) norm))
+          rotate-axis [(- (nth diff 1)) (nth diff 0) 0]
+          ]
+      (union
+       (sphere *line-radius*)
+       (translate [0 0 norm]
+                  (sphere *line-radius*))
+       (->> (cylinder *line-radius* norm)
          (translate [0 0 (/ norm 2)])
          (rotate rotate-angle rotate-axis)
          (translate from)
-         )))
+         )))))
+
+(defn lines [p & ps]
+  (apply union
+         (:path (reduce (bound-fn [{a :point, path :path} b]
+                          {:path (conj path (line a b))
+                           :point b})
+                        {:point p}
+                        ps))))
