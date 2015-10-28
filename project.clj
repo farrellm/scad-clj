6f6fbd0119d7becda282c0ea9b45bd770573de2c
Matthew Farrell <farrellm@alum.mit.edu>
2015-10-27 22:53:22 -0400

    Version 0.5.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject scad-clj "0.4.2-SNAPSHOT"
+(defproject scad-clj "0.5.0"
   :description "OpenSCAD DSL in Clojure"
   :url "https://github.com/farrellm/scad-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.match "0.2.2"]
-                 [net.mikera/core.matrix "0.32.1"]
+                 [net.mikera/core.matrix "0.42.1"]
                  ])
