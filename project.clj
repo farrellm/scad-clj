ea057d819ebae26653107046d767fc89e441aa99
munsel <munsel@munsel.de>
2019-03-26 01:12:39 +0100

    updated clojure version to 1.8

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/farrellm/scad-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.match "0.2.2"]
                  [net.mikera/core.matrix "0.42.1"]])
